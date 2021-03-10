9fc575ab9996d001e4b5f828f4ac133dbbd20286
Jean Borger <jeanmarieborger@gmail.com>
2021-03-09 20:07:39 -0500

    begin JS planning, edit copy

---
+++ main.js
@@ -0,0 +1,3 @@
+// Step 1: Create poem object to store and output user poem
+// Step 2: Write JS function to populate the html content dynamically line by line, based on poem object
+// Step 3: Write JS to show the "How to write a haiku" section to user on click of "Write a haiku!" button 
