66606de4cc811693abf3f06f739be1c4b81c1b58
Jean Borger <jeanmarieborger@gmail.com>
2021-03-21 20:11:19 -0400

    add responsive image gallery

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-// Step 1: Create poem object to store and output user poem
-// Step 2: Write JS function to populate the html content dynamically line by line, based on poem object
-// Step 3: Write JS to show the "How to write a haiku" section to user on click of "Write a haiku!" button 
+// Write function for onclick of the image, open a modal window or new page for user to write haiku
+// Write function for onclick of image, open a modal window or new page for user to write haiku
+// Write function for onclick of submit button, grab each value from each input and output it to the page in a div with the main image above.
