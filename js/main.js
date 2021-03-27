2db5750184c97720327af93a6fef9b17d93f8858
Jean Borger <jeanmarieborger@gmail.com>
2021-03-27 19:33:18 -0400

    create single input area for poem, add submit button that hides input area and displays poem

--- js/main.js
+++ js/main.js
@@ -18,5 +18,30 @@ $().ready(function(){
     })
 })
 
-// Write function for onclick of submit button, grab each value from each input and output it to the page in a div with the main image above.
+// Write function for onclick of submit button, grab value from input and output it to the page in a div with the main image above.
 
+var poemInput = document.getElementById("poem-input"); //first line entered by user
+var submitButton = document.getElementsByTagName("button")[0]; //button
+var poemToDisplay; //first line to display to page
+
+submitButton.onclick = function() {
+    $("#input-box").hide();
+    poemToDisplay = poemInput.value;
+    console.log(poemToDisplay);
+    document.getElementById("poem-display").innerHTML = poemToDisplay;
+}
+
+// $().ready(function(){
+//     $("#selected-picture-box").hide();
+//     $("#input-box").hide();
+
+//     $("img").click(function() {
+//         $("#gallery-box").css("display","none");
+//         $(".instruction1").css("display","none");
+//         imgName = $(this).attr("src");
+//         var img = document.createElement("img");
+//         img.style.width = "100%";
+//         img.setAttribute("src", imgName);
+//         $("#selected-picture-box").append(img);
+//         $("#selected-picture-box").show();
+//         $("#input-box").show();
